276cbf6bb6cd2b4db28c9a9f31ef0da44cffa9bf
Kimserey <kimserey.lam@gmail.com>
2018-12-29 17:21:00 +0000

    Added version

---
+++ Backend/Blue/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Backend - 3.1.0";
+        }
+    }
+}
